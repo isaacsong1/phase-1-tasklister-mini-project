b5701e64077e1b93b75f0fc18894eaded257c7b5
Isaac Song <isaacsongs10@gmail.com>
2023-09-13 16:15:04 -0700

    add editted files, needs button remover

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // Create li element then append in #tasks 
+    const li = document.createElement('li');
+    li.textContent = event.target.querySelector('input').value;
+
+    const btn = document.createElement('button');
+    btn.textContent = 'Delete'
+    btn.addEventListener('click', () => document.querySelector('li').remove());
+
+    document.querySelector('ul').append(li, btn);
+    // Need to remove button element
+  })
 });
