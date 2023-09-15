f40421539dba932b8c20529568aa7d10ee04a4f5
Isaac Song <isaacsongs10@gmail.com>
2023-09-15 14:09:32 -0700

    move form reset inside create func

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
     createToDoItem(event.target.querySelector('#new-task-description').value);
-    document.querySelector('form').reset();
-    // Need to remove button element
   })
 });
 
@@ -18,8 +16,10 @@ function createToDoItem(task) {
   btn.textContent = 'x'
   li.appendChild(btn);
   document.querySelector('ul').appendChild(li);
+  document.querySelector('form').reset();
 }
 
+// Delete item when button is pressed
 function deleteToDoItem(event) {
   event.target.parentNode.remove();
 }
